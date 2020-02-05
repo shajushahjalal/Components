90e8f55caae10a3206f20f5e319fe523a4dc07a3
Shahjalal <shajushahjalal@gmail.com>
2020-02-06 01:28:55 +0600

    API & WEB Message

---
+++ FileUpload.php
@@ -0,0 +1,141 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Component;
+
+/**
+ *
+ * @author Sm Shahjalal Shaju
+ */
+use Intervention\Image\Facades\Image;
+
+trait FileUpload {
+    
+    /*
+     * Define Directories
+     */
+    protected  $productImageDir = "storage/uploads/productImage/";
+    protected  $logoImageDir = "storage/uploads/logo/";
+    protected  $sliderImageDir = "storage/uploads/slider/";
+    protected  $categoryImageDir = "storage/uploads/category/";
+    protected  $featureImageDir = "storage/uploads/feature/";
+    protected  $userImageDir    = "storage/uploads/users/";
+    protected  $contentImage    = "storage/uploads/pages/";
+    protected  $brandingImage    = "storage/uploads/brands/";
+
+
+    /*
+     * ---------------------------------------------
+     * Check the Derectory If exists or Not
+     * ---------------------------------------------
+     */
+    protected function CheckDir($dir){
+        if(!is_dir($dir)){
+            mkdir($dir,0777,true);
+        }
+        
+        if(!file_exists($dir.'index.php')){
+            $file = fopen($dir.'index.php','w');
+            fwrite($file," <?php \n /* \n Unauthorize Access \n @Developer: Sm Shahjalal Shaju \n Email: shajushahjalal@gmail.com \n */ ");
+            fclose($file);
+        }
+    }
+    
+    /*
+     * ---------------------------------------------
+     * Check the file If exists then Delete the file
+     * ---------------------------------------------
+     */
+    protected function RemoveFile($filePath) {
+        if(file_exists($filePath)){
+            unlink($filePath);
+        }
+    }
+    
+    /*
+     * ---------------------------------------------
+     * Upload an Image
+     * Change Image height and width
+     * Send the null value in height or width to keep 
+     * the Image Orginal Ratio.
+     * ---------------------------------------------
+     */
+    protected function UploadImage($request,$fileName,$dir,$width,$height,$oldFile){
+        if(!$request->hasFile($fileName)){
+            return $oldFile;
+        }
+        $this->CheckDir($dir);
+        $this->RemoveFile($oldFile);
+        
+        ini_set('memory_limit', '1024M');
+        $image = $request->file($fileName);
+        $filename = $fileName.'_'.time().'.'.$image->getClientOriginalExtension();
+        $path = $dir.$filename;
+        if( empty($width) ){
+            Image::make($image)->resize(null,$height,function($constant){
+                $constant->aspectRatio();
+            })->save($path);
+        }
+        elseif( empty($height) ){
+            Image::make($image)->resize($width,null,function($constant){
+                $constant->aspectRatio();
+            })->save($path);
+        }else{
+            Image::make($image)->resize($width,$height)->save($path);
+        }
+        
+        return $path;
+    }
+
+
+
+    
+    /*
+     * ---------------------------------------------
+     * Upload any Kind of file
+     * ---------------------------------------------
+     */
+    protected function UploadVideo($request,$fileName,$dir,$oldFile){
+        if(!$request->hasFile($fileName)){
+            return $oldFile;
+        }
+        ini_set('memory_limit', '1024M');
+        $this->CheckDir($dir);
+        $this->RemoveFile($oldFile); 
+        $file = $request->file($fileName);  
+        $Newfilename = 'video_'.time().'.mp4';
+        $file->move($dir,$Newfilename); 
+        return $dir.$Newfilename;
+    }
+    
+    /**
+     * ------------------------------------------------------------
+     * Upload Multiple Image
+     * ------------------------------------------------------------
+     */
+    protected function UploadMultipleImage($request,$fileName,$dir,$width,$height) {
+        if($request->hasfile($fileName))
+        {
+            $this->CheckDir($dir);
+            ini_set('memory_limit', '1024M');
+            $count = 0;
+            $allImage= [];
+            foreach($request->file($fileName) as $image)
+            {
+                $filename = $fileName.$count.time().'.'.$image->getClientOriginalExtension();
+                $path = $dir.$filename;
+                Image::make($image)->resize($width,$height)->save($path);
+                $allImage[$count] = $path;
+                $count++;
+            }
+            return $allImage;
+        }
+    }
+    
+    
+}
---
+++ Message.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Component;
+
+trait Message{
+    
+    protected $status = false;
+    protected $message = '';   
+    protected $reset = false; 
+    protected $modal = false;
+    protected $table = false;    
+    protected $button = false;
+    protected $api_token = "";
+    protected $data = [];
+    
+
+    /**
+     * Return Default Output Message
+     * This Method for web JSON Response
+     */
+    protected function output(){
+        return [ 
+            'status' => $this->status, 'message' => $this->message, 'reset' => $this->reset,
+            'table' => $this->table, 'modal' => $this->modal, 'button' => $this->button
+        ];
+    }
+
+    /**
+     *  Success  Function Set the Value as Success
+     * This Method for Web Success Message
+     */
+    protected function success( $smg = null, $reset = true, $modal = true, $table = true, $button = false){
+        $this->status = true;
+        $this->message = $smg == null ? !empty($this->message) ? $this->message : 'Information Save Successfully' : $smg ;
+        $this->reset = $reset;
+        $this->modal = $modal;
+        $this->table = $table;
+        $this->button = $button;
+    }
+
+    /**
+     * Success  Function For API
+     * Set api response status as Success
+     * This Method is responsible all API Response
+     */
+    protected function apiSuccess($message = Null, $data = Null){
+        $this->status = true;
+        $this->message = !empty($message) ? $message : 'Successfully';
+        $this->data = $data;
+    }
+
+    /**
+     * Return Default API Output Message
+     * This Method for API Response
+     */
+    protected function apiOutput(){
+        return [
+            'status'    => $this->status,       'message'   => $this->message,
+            'api_token' => $this->api_token,    'data'      => $this->data
+        ];
+    }
+
+    /**
+     * Get Error Message
+     * If Application Environtment is local then
+     * Return Error Message With filename and Line Number
+     * else return a Simple Error Message
+     */
+    protected function getError($e = null){
+        if( strtolower(env('APP_ENV')) == 'local' && !empty($e) ){
+            return $e->getMessage() . ' On File ' . $e->getFile() . ' on line ' . $e->getLine();
+        }
+        return 'Something went wrong!';
+    }
+
+    
+
+    /**
+     * Get Validation Error
+     */
+    public function getValidationError($validator){
+        if( strtolower(env('APP_ENV')) == 'local' ){
+            return $validator->errors()->first();
+        }
+        return 'Data Validation Error';
+    }
+}
